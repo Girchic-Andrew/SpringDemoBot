73aafe7a20131445bd97169fcf6b2bc237913c5d
unknown <x4txgk@proton.me>
2022-08-01 17:26:24 +0300

    first commit

---
+++ src/main/java/service/TelegramBot.java
@@ -0,0 +1,66 @@
+package service;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import config.BotConfig;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot{
+
+	final BotConfig config;
+	public TelegramBot(BotConfig config) {
+		this.config = config;
+	}
+	
+	@Override
+	public String getBotUsername() {
+		return config.getBotName();
+	}
+
+	@Override
+	public String getBotToken() {
+		return config.getToken();
+	}
+	
+	@Override
+	public void onUpdateReceived(Update update) {
+		
+		if(update.hasMessage() && update.getMessage().hasText()) {
+			String messageText = update.getMessage().getText();
+			long chatId = update.getMessage().getChatId();
+			switch(messageText) {
+				case "/start":
+					startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+					break;
+				
+				default: sendMessage(chatId, "Sorry, command wasn't recognized...");
+					
+			}
+			
+		}
+	}
+	
+	private void startCommandReceived(long chatId, String firstName) {
+		String answer = "Hi, " + firstName + " nice to meet you!";
+		sendMessage(chatId, answer);
+	}
+
+	private void sendMessage(long chatId, String textToSend) {
+		SendMessage message = new SendMessage();
+		message.setChatId(String.valueOf(chatId));
+		message.setText(textToSend);
+		
+		try {
+			execute(message);
+		}
+		catch(TelegramApiException e) {
+			
+			
+		}
+	}
+	
+}
